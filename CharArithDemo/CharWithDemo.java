8397bd67f01fb014e05be0982fa45485ae804235
Daniel Geovanovich <geodj002@mymail.unisa.edu.au>
2023-11-21 21:56:15 +1030

    Java learning programs

---
+++ CharArithDemo/CharWithDemo.java
@@ -0,0 +1,16 @@
+// Character variables can be handled like integers
+
+class CharWithDemo {
+  public static void main(String[] args) {
+    char ch;
+
+    ch = 'X';
+    System.out.println("ch contains " + ch);
+
+    ch++; // increment char
+    System.out.println("ch is now " + ch);
+
+    ch = 90; // give ch the value Z
+    System.out.println("ch is now " + ch);
+  }
+}
