95777d7c025316521873f4c4210fb89ecc405664
Daniel Geovanovich <geodj002@mymail.unisa.edu.au>
2023-11-26 18:37:01 +1030

    latest textbook scripts

---
+++ DynInit/DynInit.java
@@ -0,0 +1,13 @@
+// Demonstrate dynamic initialisation.
+
+class DynInit {
+  public static void main(String[] args) {
+    double radius = 4, height = 5;
+
+    // dynamically initialise volume
+    double volume = 3.1416 * radius * radius * height;
+
+    System.out.println("Volume is " + volume);
+  }
+}
+
